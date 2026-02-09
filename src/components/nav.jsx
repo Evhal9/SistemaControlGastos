cb748758356df5c18f550020db9eb4411a9e4922
Mely Salerno <melysalernoflores@gmail.com>
2026-02-08 22:11:17 -0300

    mejoras al nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,13 +1,26 @@
 import { NavLink } from 'react-router-dom'
+import { useEffect, useState } from "react"
 
 function Nav() {
+  const [scrolled, setScrolled] = useState(false)
+
+    useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 20)
+    }
+
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
   return (
-    <nav className="fixed top-0 left-0 w-screen h-11 bg-[#a2d2ff]">
+    <nav className={`fixed top-0 left-0 w-screen h-11 transition-all
+    ${scrolled ? "bg-[#c96928]" : "bg-transparent"}
+  `}>
       <ul className="flex h-full">
         <li className="flex-1">
           <NavLink
             to="/"
-            className="flex h-full w-full items-center justify-center font-light text-center hover:bg-[#bde0fe] hover:text-[#ffafcc]"
+            className="flex h-full w-full items-center justify-center font-light text-center hover:bg-[#ee9c21] hover:text-[#e9d7a5]"
           >
             Inicio
           </NavLink>
@@ -16,7 +29,7 @@ function Nav() {
         <li className="flex-1">
           <NavLink
             to="/control"
-            className="flex h-full w-full items-center justify-center font-light text-center hover:bg-[#bde0fe] hover:text-[#ffafcc]"
+            className="flex h-full w-full items-center justify-center font-light text-center hover:bg-[#ee9c21] hover:text-[#e9d7a5]"
           >
             Control
           </NavLink>
@@ -25,7 +38,7 @@ function Nav() {
         <li className="flex-1">
           <NavLink
             to="/balance"
-            className="flex h-full w-full items-center justify-center font-light text-center hover:bg-[#bde0fe] hover:text-[#ffafcc]"
+            className="flex h-full w-full items-center justify-center font-light text-center hover:bg-[#ee9c21] hover:text-[#e9d7a5]"
           >
             Balance
           </NavLink>
@@ -34,7 +47,7 @@ function Nav() {
         <li className="flex-1">
           <NavLink
             to="/qa"
-            className="flex h-full w-full items-center justify-center font-light text-center hover:bg-[#bde0fe] hover:text-[#ffafcc]"
+            className="flex h-full w-full items-center justify-center font-light text-center hover:bg-[#ee9c21] hover:text-[#e9d7a5]"
           >
             QA
           </NavLink>
@@ -43,7 +56,7 @@ function Nav() {
         <li className="flex-1">
           <NavLink
             to="/login"
-            className="flex h-full w-full items-center justify-center font-light text-center hover:bg-[#bde0fe] hover:text-[#ffafcc]"
+            className="flex h-full w-full items-center justify-center font-light text-center hover:bg-[#ee9c21] hover:text-[#e9d7a5]"
           >
             Iniciar sesión
           </NavLink>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,38 @@
+import banner from '../assets/banner.jpg'
+
 function Home() {
+
+
   return (
     <>
-   <h1>Home </h1>
-   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate nemo distinctio praesentium consequuntur odio id maxime a delectus quo consectetur quas sunt, quasi harum veniam voluptates voluptate, quos, officiis perspiciatis?</p> 
-</>
-)}
+      <div className="
+        h-[80vh]
+        bg-cover
+        bg-center
+        flex
+        flex-col
+        items-center
+        justify-center
+        text-center
+        gap-4
+      " style={{ backgroundImage: `url(${banner})` }}>
+        <p
+          className="text-5xl font-semibold text-transparent"
+          style={{ WebkitTextStroke: '2px #5356a0ff' }}
+        >
+          Controlá tus gastos
+          <br />
+          y mejorá tu economía personal
+        </p>
 
-export default Home
+        <p className="text-lg text-black max-w-xl">
+          Registrá tus gastos diarios, analizá tus consumos y tomá mejores decisiones financieras
+        </p>
+      </div>
+
+      <div className="h-8"></div>
+    </>
+  );
+}
+
+export default Home;
